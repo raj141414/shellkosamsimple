20399b564b54518507c8b428c8bf353d4ae29329
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2023-10-01 12:11:38 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -117,7 +117,7 @@ def get_progress_bar_string(pct):
         pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
     cFull = int(p // 10)
-    p_str = '█' * cFull
+    p_str = '▓' * cFull
     p_str += '▒' * (10 - cFull)
     return f"{p_str}"
 
