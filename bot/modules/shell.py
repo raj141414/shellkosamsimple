a895a99efb1f91554351cdc8e0b7bbe0b4616a3c
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2023-10-01 12:05:21 +0530

    Update shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -37,6 +37,6 @@ async def shell(_, message):
 
 
 bot.add_handler(MessageHandler(shell, filters=command(
-    BotCommands.ShellCommand) & CustomFilters.owner))
+    BotCommands.ShellCommand) & CustomFilters.authorized))
 bot.add_handler(EditedMessageHandler(shell, filters=command(
-    BotCommands.ShellCommand) & CustomFilters.owner))
+    BotCommands.ShellCommand) & CustomFilters.authorized))
